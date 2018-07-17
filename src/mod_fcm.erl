1b68785f09411d35fc4079e8a89d5bc96481c55a
doomsower <K.Kuznetcov@gmail.com>
2018-07-17 19:30:46 +0300

    Messing with hook

--- src/mod_fcm.erl
+++ src/mod_fcm.erl
@@ -10,37 +10,37 @@
 -include("logger.hrl").
 -include("xmpp.hrl").
 
-start(Host, _Opts) ->
-  ?INFO_MSG("mod_fcm staring...", []),
-  FcmKey = os:getenv("FCM_KEY"),
-  fcm_app:start(normal, []),
-  ?INFO_MSG("mod_fcm key = ~s", [FcmKey]),
-  %% mod_push has priority of 50
-  ejabberd_hooks:add(store_offline_message, Host, ?MODULE, offline_message_hook, 40),
-  ?INFO_MSG("mod_fcm added offline hook", []),
-  fcm:start(swapp_fcm, FcmKey),
-  ?INFO_MSG("mod_fcm started", []),
-  ok.
-
-offline_message_hook(#message{to = To, from = From, body = Body} = Acc) ->
+offline_message(#message{to = To, from = From, body = Body} = Acc) ->
   ToResource = To#jid.lresource,
   FromUser = From#jid.luser,
   Message = xmpp:get_text(Body),
-  ?INFO_MSG("mod_fcm pushing, '~s' from '~s' to '~s'", [Message, FromUser, ToResource]),
+  ?DEBUG("mod_fcm pushing, '~s' from '~s' to '~s'", [Message, FromUser, ToResource]),
   
   if
     ToResource /= <<>> ->
       fcm:push(swapp_fcm, [ToResource], [{<<"data">>, [{<<"message">>, Message}]}]),
-      ?INFO_MSG("mod_fcm pushed, '~s' from '~s' to '~s'", [Message, FromUser, ToResource]),
+      ?DEBUG("mod_fcm pushed, '~s' from '~s' to '~s'", [Message, FromUser, ToResource]),
       ok;
     true -> ok
   end,
   
   Acc.
 
+start(Host, _Opts) ->
+  ?INFO_MSG("mod_fcm staring...", []),
+  FcmKey = os:getenv("FCM_KEY"),
+  fcm_app:start(normal, []),
+  ?INFO_MSG("mod_fcm key = ~s", [FcmKey]),
+  %% mod_push has priority of 50
+  ejabberd_hooks:add(offline_message_hook, Host, ?MODULE, offline_message, 45),
+  ?INFO_MSG("mod_fcm added offline hook", []),
+  fcm:start(swapp_fcm, FcmKey),
+  ?INFO_MSG("mod_fcm started", []),
+  ok.
+
 stop(Host) ->
   ?INFO_MSG("mod_fcm stoping...", []),
-  ejabberd_hooks:delete(store_offline_message, Host, ?MODULE, offline_message_hook, 40),
+  ejabberd_hooks:delete(offline_message_hook, Host, ?MODULE, offline_message, 45),
   ?INFO_MSG("mod_fcm stopping fcm service...", []),
   fcm:stop(swapp_fcm),
   ?INFO_MSG("mod_fcm stopped", []),
