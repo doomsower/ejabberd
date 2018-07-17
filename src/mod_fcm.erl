054e75bc85a9f2511c1940deeae4e5962f89fb6e
doomsower <K.Kuznetcov@gmail.com>
2018-07-17 14:40:39 +0300

    fcm_sup:start_link().

--- src/mod_fcm.erl
+++ src/mod_fcm.erl
@@ -12,8 +12,7 @@
 
 start(Host, _Opts) ->
   ?INFO_MSG("mod_fcm staring...", []),
-  AllEnv = os:getenv(),
-  ?INFO_MSG("mod_fcm env = ~p", [AllEnv]),
+  fcm_sup:start_link(),
   FcmKey = os:getenv("FCM_KEY"),
   ?INFO_MSG("mod_fcm key = ~s", [FcmKey]),
   %% mod_mam has priority of 50
